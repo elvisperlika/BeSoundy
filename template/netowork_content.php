efa955c99e5d4f3862d86785dad79d0203b883bf
Noodles <elvis.perlika@studio.unibo.it>
2024-04-04 13:42:50 +0200

    Add href to profiles on network pages

--- template/netowork_content.php
+++ template/netowork_content.php
@@ -3,12 +3,12 @@
         if($_SESSION['side'] == "followers"){
             $followers = $dbh->getFollowers($_SESSION['sessionUser']);
             foreach($followers as $user){
-                echo "<span> ".$user["follower"]." </span>";
+                echo "<a href='profile.php?user=".$user["follower"]."'>".$user["follower"]." </a>";
             }
         } else {
             $following = $dbh->getFollowing($_SESSION['sessionUser']);
             foreach($following as $user){
-                echo "<span> ".$user["followed"]." </span>";
+                echo "<a href='profile.php?user=".$user["followed"]."'>".$user["followed"]." </a>";
             }
         }
     ?>
