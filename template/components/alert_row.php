83fd1d3480719bb862cc560dc606475006cd268d
Noodles <elvis.perlika@studio.unibo.it>
2024-04-09 17:58:49 +0200

    Add time in alert row

--- template/components/alert_row.php
+++ template/components/alert_row.php
@@ -1,5 +1,19 @@
 <?php
     echo "<li>";
+        echo "<span>";
+        $time = $alert["time"];
+        $current_time = time();
+        $time_diff = $current_time - strtotime($time);
+        if ($time_diff < 60) {
+            echo $time_diff . " seconds ago";
+        } elseif ($time_diff < 3600) {
+            echo floor($time_diff / 60) . " minutes ago";
+        } elseif ($time_diff < 86400) {
+            echo floor($time_diff / 3600) . " hours ago";
+        } else {
+            echo floor($time_diff / 86400) . " days ago";
+        }
+        echo "</span>";
         echo "<a href='profile.php?user=".$alert["sender"]."'>".$alert["sender"]." </a>";
         switch ($alert["type"]) {
             case 'LIKE_POST':
@@ -8,7 +22,6 @@
                 break;
             case 'LIKE_COMMENT':
                 echo "<span>liked your comment</span>";
-
                 echo "<a class='elementBtn' href='#".$alert["idElement"]."'>show</a>";
                 break;
             case 'COMMENT_POST':
